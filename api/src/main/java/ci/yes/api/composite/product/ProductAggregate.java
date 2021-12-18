a3bddfcfc699f2261e2e49f6a4562a7bbbc03ca7
soungalo <soungaloyeo@gmail.com>
2021-12-18 15:12:52 +0000

    mongoDB and msql db config  added in yml & dockers files

--- api/src/main/java/ci/yes/api/composite/product/ProductAggregate.java
+++ api/src/main/java/ci/yes/api/composite/product/ProductAggregate.java
@@ -10,13 +10,22 @@ public class ProductAggregate {
   private final List<ReviewSummary> reviews;
   private final ServiceAddresses serviceAddresses;
 
+  public ProductAggregate() {
+    productId = 0;
+    name = null;
+    weight = 0;
+    recommendations = null;
+    reviews = null;
+    serviceAddresses = null;
+  }
+
   public ProductAggregate(
-    int productId,
-    String name,
-    int weight,
-    List<RecommendationSummary> recommendations,
-    List<ReviewSummary> reviews,
-    ServiceAddresses serviceAddresses) {
+          int productId,
+          String name,
+          int weight,
+          List<RecommendationSummary> recommendations,
+          List<ReviewSummary> reviews,
+          ServiceAddresses serviceAddresses) {
 
     this.productId = productId;
     this.name = name;
